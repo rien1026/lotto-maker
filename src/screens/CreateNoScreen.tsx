44e7f48de2e8e661bc0beb162ee3975932ce71e9
SangyeonCho <rien1026@gmail.com>
2020-09-02 20:21:09 +0900

    screen

--- src/screens/CreateNoScreen.tsx
+++ src/screens/CreateNoScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { NavigationSwitchProp } from 'react-navigation';
-import { View, FlatList, Text, TouchableWithoutFeedback } from 'react-native';
+import { FlatList, Text, TouchableWithoutFeedback, ScrollView, View, StyleSheet } from 'react-native';
 import LottieView from 'lottie-react-native';
 
 type Props = { navigation: NavigationSwitchProp };
@@ -15,16 +15,14 @@ export default class CreateWinNoScreen extends React.Component<Props, State> {
 
 		this.state = {
 			winNoList: [],
-			candidateNoList: [{ 1: 'n' }],
+			candidateNoList: ['n', 'i', 'e'],
 		};
 	}
 
 	setCandidateNoList = () => {
 		let candidateNoList = [];
 		for (let i = 1; i < 46; i++) {
-			let candidateNo = {};
-			candidateNo[i] = 'n';
-			candidateNoList.push(candidateNo);
+			candidateNoList[i - 1] = 'n';
 		}
 
 		this.setState({ candidateNoList });
@@ -35,9 +33,12 @@ export default class CreateWinNoScreen extends React.Component<Props, State> {
 	}
 
 	createWinNo = () => {
-		let winNoList = [];
+		let winNoList = this.getInclusionNoList();
 		let winNoMap = {};
 		let whileLimit = 0;
+
+		let exclusionNoMap = this.getExclusionNoMap();
+
 		while (winNoList.length < 8) {
 			// for avoid loop
 			whileLimit++;
@@ -47,15 +48,35 @@ export default class CreateWinNoScreen extends React.Component<Props, State> {
 
 			let winNo = Math.ceil(Math.random() * 45);
 
-			if (winNoMap[winNo]) {
+			if (winNoMap[winNo] || exclusionNoMap[winNo]) {
 				continue;
 			}
 
 			winNoMap[winNo] = 1;
 			winNoList.push(winNo);
 		}
+	};
 
-		console.log(winNoList);
+	getInclusionNoList = () => {
+		let inclusionNoList = [];
+		for (let i = 0; i < this.state.candidateNoList.length; i++) {
+			if (this.state.candidateNoList.length[i] !== 'i') continue;
+
+			inclusionNoList.push(i);
+		}
+
+		return inclusionNoList;
+	};
+
+	getExclusionNoMap = () => {
+		let exclusionNoMap = {};
+		for (let i = 0; i < this.state.candidateNoList.length; i++) {
+			if (this.state.candidateNoList.length[i] !== 'e') continue;
+
+			exclusionNoMap[i] = 1;
+		}
+
+		return exclusionNoMap;
 	};
 
 	setNoFlag = (index: number, flag: 'i' | 'e' | 'n') => {
@@ -65,25 +86,48 @@ export default class CreateWinNoScreen extends React.Component<Props, State> {
 
 	render() {
 		return (
-			<View>
-				<LottieView source={require('../assets/3407-coinpig.json')} loop style={{ width: 300, height: 300 }} />
-				<FlatList
-					data={this.state.candidateNoList}
-					renderItem={(item) => (
-						<View>
+			<ScrollView style={{ width: '100%', flex: 1 }}>
+				<View style={{ flex: 1, alignItems: 'center', backgroundColor: '#ebebeb', borderWidth: 1 }}>
+					<LottieView source={require('../assets/3407-coinpig.json')} loop style={{ width: 300, height: 300 }} />
+				</View>
+				<View style={style.winNoListBox}>
+					{this.state.candidateNoList.map((item, index) => (
+						<View style={{ width: 50, height: 50, backgroundColor: 'red' }} key={index}>
 							<TouchableWithoutFeedback
 								onPress={() => {
-									this.setNoFlag(item.index + 1, 'i');
+									this.setNoFlag(item.index, 'i');
 								}}
 							>
-								<Text>{item.index + 1}</Text>
+								<Text>{index + 1}</Text>
 							</TouchableWithoutFeedback>
 						</View>
-					)}
-					numColumns={5}
-					keyExtractor={(item) => JSON.stringify(item)}
-				/>
-			</View>
+					))}
+				</View>
+				<View style={style.winNoListBox}>
+					{this.state.candidateNoList.map((item, index) => (
+						<View style={{ width: 50, height: 50, backgroundColor: 'blue' }} key={index}>
+							<TouchableWithoutFeedback
+								onPress={() => {
+									this.setNoFlag(item.index, 'e');
+								}}
+							>
+								<Text>{index + 1}</Text>
+							</TouchableWithoutFeedback>
+						</View>
+					))}
+				</View>
+			</ScrollView>
 		);
 	}
 }
+
+const style = StyleSheet.create({
+	winNoListBox: {
+		flex: 1,
+		alignItems: 'center',
+		backgroundColor: '#ebebeb',
+		borderWidth: 1,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+	},
+});
